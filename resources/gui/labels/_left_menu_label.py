57448411a5d608adb0d1bd2cb65fa8d0edb50a02
a5892731 <a5892731@o2.pl>
2022-05-11 18:19:10 +0200

    basic template of tkinter gui

--- resources/gui/labels/_left_menu_label.py
+++ resources/gui/labels/_left_menu_label.py
@@ -1,27 +1,26 @@
 from tkinter import *
 
+from resources.gui.widgets.button import button
+
 def left_menu_bar(self):
-
-    def draw_button(label, name, function, row = 0):
-        button = Button(label, font=("Arial", 12, "bold"), text=name, command=function)
-        button.grid(column=0, row=row, sticky="we")
-        return button
     '''--------------------------------------------------------------------------------------------------------------'''
-    self.left_menu_label.columnconfigure(0, minsize=100)
 
-    draw_button(label=self.left_menu_label, name="", function="", row=0)  # row space
+    menu = LabelFrame(self.left_menu_label, text="MENU", background="#cfd1cf", bd=5)
+    menu.grid(column=1, row=0, sticky="nesw")
 
-    draw_button(label=self.left_menu_label, name="Side 1", function=self.view_side_1, row = 1)
-    draw_button(label=self.left_menu_label, name="Side 2", function=self.view_side_2, row = 2)
+    button(label=menu, text="", command="", column=0, row=0)  # row space
 
-    draw_button(label=self.left_menu_label, name="", function="", row=3)  # row space
-    draw_button(label=self.left_menu_label, name="", function="", row=4)  # row space
-    draw_button(label=self.left_menu_label, name="", function="", row=5)  # row space
-    draw_button(label=self.left_menu_label, name="", function="", row=6)  # row space
-    draw_button(label=self.left_menu_label, name="", function="", row=7)  # row space
-    draw_button(label=self.left_menu_label, name="", function="", row=8)  # row space
+    button(label=menu, text="Side 1", command=self.view_side_1, column=0, row=1)
+    button(label=menu, text="Side 2", command=self.view_side_2, column=0, row=2)
 
-    draw_button(label=self.left_menu_label, name="Exit", function=self.exit_program, row = 9)
+    button(label=menu, text="", command="", column=0, row=3)  # row space
+    button(label=menu, text="", command="", column=0, row=4)  # row space
+    button(label=menu, text="", command="", column=0, row=5)  # row space
+    button(label=menu, text="", command="", column=0, row=6)  # row space
+    button(label=menu, text="", command="", column=0, row=7)  # row space
+    button(label=menu, text="", command="", column=0, row=8)  # row space
+
+    button(label=menu, text="Exit", command=self.exit_program, column=0, row=9)
 '''------------------------------------------------------------------------------------------------------------------'''
 def view_side_1(self):
     self.main_label.destroy()
