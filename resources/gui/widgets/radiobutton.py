0e29b6599133047543b31d1b34193c03d0c80464
jakubz <a5892731@o2.pl>
2022-11-29 14:56:32 +0100

    CSSD template

--- resources/gui/widgets/radiobutton.py
+++ resources/gui/widgets/radiobutton.py
@@ -10,21 +10,35 @@ value is a lists of variable values
 
 
 
-def radiobuttons(label, variable, row, command, text, value, column=0, state=NORMAL):
+def radiobuttons(label, variable, row, command, text, value, column=0, state=NORMAL, select=0):
     #text and value are lists
+    #select is a number of radiobutton to be selected
 
+
+    r=list()
     size = len(text)
 
     for row_ in range(size):
-        r = Radiobutton(label, font=("Arial", 10), text=text[row_], command=command,
+        r=Radiobutton(label, font=("Arial", 10), text=text[row_], command=command,
                          variable=variable, value=value[row_], background="#cfd1cf")
         r.grid(column=column, row=row+row_, sticky="w")
 
+        if row_ == select:
+            r.select()
+        else:
+            r.deselect()
 
-def radiobutton(label, name, variable, row, column=0, command = "", value = True):
+
+
+def radiobutton(label, name, variable, row, column=0, command = "", value = True, select = False):
     c = Radiobutton (label, font=("Arial", 12), text=name,
                     variable=variable, value=value,
                     height=0,
                     width=0, background="#cfd1cf",
                     )
     c.grid(column=column, row=row, sticky="wns")
+
+    if select:
+        c.select()
+    else:
+        c.deselect()
